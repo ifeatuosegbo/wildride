f269ef91acacf949bc13ae4550b7b3c1a72c5f77
Ifeatu Osegbo <166733358+ifeatuosegbo@users.noreply.github.com>
2025-10-07 16:47:22 +0100

    Update config.js
    Updated

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_Tu8jKdUg2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1n9e3d8qnoe14vqsb3mehv7ja4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'eu-north-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
