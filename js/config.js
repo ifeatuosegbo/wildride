d82c1b6aa109dcd743f4d7b61b96970555cdf989
Ifeatu Osegbo <166733358+ifeatuosegbo@users.noreply.github.com>
2025-10-08 07:14:41 +0100

    Update config.js
    Update with SPA

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_Tu8jKdUg2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1n9e3d8qnoe14vqsb3mehv7ja4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_Dvjl4B4lq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1846mup2qrg60efbrvml5tet24', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'eu-north-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
